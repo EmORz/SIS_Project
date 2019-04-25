3da45304eaaf7d32a356d4f2d3b3204633a708ad
Emo <eiv.razgrad@gmail.com>
2019-04-25 18:44:59 +0300

    make to ParseRequest()

---
+++ SIS.HTTP/Exceptions/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace SIS.HTTP.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        private const string ErrorMessage = "The Request was malformed or contains unsupported elements.";
+
+        public HttpStatusCode StatusCode = HttpStatusCode.BadRequest;
+
+        public BadRequestException() 
+            : base(ErrorMessage)
+        {
+            
+        }
+
+    }
+}
---
+++ SIS.HTTP/Requests/HttpRequest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Headers.Contracts;
+using SIS.HTTP.Requests.Contracts;
+
+namespace SIS.HTTP.Requests
+{
+    public class HttpRequest : IHttpRequest
+    {
+        public HttpRequest(string requestStringPath)
+        {
+            this.FormData = new Dictionary<string, object>();
+            this.QueryData = new Dictionary<string, object>();
+            this.Headers = new HttpHeaderCollection();
+
+            this.ParseRequest(requestStringPath);
+        }
+
+        private void ParseRequest(string requestStringPath)
+        {
+            //Todo make buisness logic
+            throw new System.NotImplementedException();
+        }
+
+        public string Path { get; private set; }
+        public string Url { get; private set; }
+        public Dictionary<string, object> FormData { get; }
+        public Dictionary<string, object> QueryData { get; }
+        public IHttpHeaderCollection Headers { get; }
+        public HttpRequestMethod RequestMethod { get; private set; }
+    }
+}
